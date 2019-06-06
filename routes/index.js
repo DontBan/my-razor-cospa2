0c110235ddb658f159eb02f9dfb3b0debfde003e
Kobayashi Tomohiro <17561732+DontBan@users.noreply.github.com>
2019-06-06 02:24:11 +0000

    作業開始

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ヒゲソリ替刃コスパ計算' });
 });
 
+router.post('/', function (req, res, next) {
+  // ユーザーの入力を受け取って、クッキーに入れる
+  res.render('result', { title: 'ヒゲソリ替刃コスパ計算:結果' });
+});
+
 // 表示確認用
 router.get('/result', function (req, res, next) {
   res.render('result', { title: 'ヒゲソリ替刃コスパ計算結果' });
-})
+});
 
 module.exports = router;
