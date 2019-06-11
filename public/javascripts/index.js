caab62f6f34128dc35ce01fe71d4830f36c34704
Kobayashi Tomohiro <17561732+DontBan@users.noreply.github.com>
2019-06-11 05:42:37 +0000

    #5 入力を受け取る処理を実装しました。

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,4 +10,11 @@
   $('#start-someday').click(function () {
     $('#someday-form').slideToggle(500);
   });
+
+  // 入力フォームのdateの最大値を今日に設定
+  var date = new Date();
+  var yyyy = date.getFullYear();
+  var mm = ('0' + (date.getMonth() + 1)).slice(-2);
+  var dd = ('0' + date.getDate()).slice(-2);
+  document.getElementById('start-date').max = yyyy + '-' + mm + '-' + dd;
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const moment = require('moment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ヒゲソリ替刃コスパ計算' });
 });
 
 router.post('/', function (req, res, next) {
-  // ユーザーの入力を受け取って、クッキーに入れる
+  // ユーザーの入力を受け取って、オブジェクトに入れる
+  const toCalc = {};
+  if (req.body.button1) {
+    toCalc.price = req.body.price1;
+    // 今日の日付
+    toCalc.start = moment().format('YYYY-MM-DD');
+  } else {
+    toCalc.price = req.body.price2;
+    toCalc.start = req.body.date;
+  }
+  
+
+  console.log(toCalc);
+
+  // クッキーに入れる
+
   res.render('result', { title: 'ヒゲソリ替刃コスパ計算:結果' });
 });
 
