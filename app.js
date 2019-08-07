b1be9d11ec0449ce68661bbadbade7cdc5006e7a
Kobayashi Tomohiro <17561732+DontBan@users.noreply.github.com>
2019-08-07 02:25:55 +0000

    helmetを利用開始

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
